15789f3e3d5a39705d8fcf6face95444a94100e2
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-11-28 17:35:46 -0800

    Practicing index_by with dummy data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_011926) do
+ActiveRecord::Schema.define(version: 2018_11_29_011417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2018_11_02_011926) do
     t.text "tweeet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
   end
 
 end
